0fbdd0261fca5a4d3336174a5d6db3347cacb2a0
morysek <moravec.galaxy@seznam.cz>
2025-10-20 21:35:29 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// File: src/App.jsx
 import React from 'react';
 import './App.css';
 
@@ -25,9 +24,34 @@ Czechia's only<br />STEM Racing<br />team
 </div>
 
 
-<div className="text text--three">
-Scroll
-</div>
+{/* Placeholder SVG 1: bottom-right, height = 1/3 of viewport, width = viewport/1.3 */}
+<svg
+className="placeholder placeholder--one"
+viewBox="0 0 100 100"
+preserveAspectRatio="none"
+aria-hidden
+>
+<rect x="0" y="0" width="100" height="100" />
+</svg>
+
+
+{/* Placeholder SVG 2: bottom-left, height = 120% of placeholder1 height, width = height * 2.1 */}
+<svg
+className="placeholder placeholder--two"
+viewBox="0 0 100 100"
+preserveAspectRatio="none"
+aria-hidden
+>
+<rect x="0" y="0" width="100" height="100" />
+</svg>
+
+
+{/* Third text aligned to the top-left of placeholder one */}
+<div className="text text--three">Scroll</div>
+
+
+{/* Black 3px line positioned 1/30 of the site below the first text */}
+<div className="line" aria-hidden />
 </div>
 </section>
 ))}

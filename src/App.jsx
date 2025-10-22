c41ebf6186cb53137ba6bcee65e9929fd0710659
morysek <moravec.galaxy@seznam.cz>
2025-10-22 21:27:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -259,7 +259,7 @@ React.useEffect(() => {
               Czechia's only<br />STEM Racing<br />team
             </div>
           </div>
-          <img src="/muzschodyweb.svg" alt="" className="img--two" aria-hidden />
+          <img src="/schody.svg" alt="" className="img--two" aria-hidden />
           <div className="text text--three" aria-hidden>
               Scroll
           </div>

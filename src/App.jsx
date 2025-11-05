470b3a65994e6e747fe0912a68953e32f9c79094
morysek <moravec.galaxy@seznam.cz>
2025-11-05 18:55:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -483,13 +483,13 @@ React.useEffect(() => {
       <div className="inner">
         <div className="line" aria-hidden />
         <div className="car-line" aria-hidden></div>
-        <img src="/pruhmuzweb.svg" alt="" className="pruh-img" aria-hidden />
       </div>
       {/* car-wrap must be a direct child of .page so JS can position it relative to the page */}
       <div className="car-wrap" aria-hidden>
         <span className="car-text">The Car</span>
         <span className="car-num">1</span>
       </div>
+      <img src="/pruhmuzweb.svg" alt="" className="pruh-img" aria-hidden />
       {/* --- inner-second: anchored left/right like .inner, top:18%, bottom:var(--border) */}
       <div className="inner-second" aria-hidden>
         {/* left-box (kept where you had it previously) */}

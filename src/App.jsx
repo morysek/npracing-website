5b2bea27b6df31f31b31e3426a5ca35cedfa7bb8
morysek <moravec.galaxy@seznam.cz>
2025-11-11 20:44:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -562,7 +562,7 @@ if (page.matches(':nth-of-type(3)')) {
         Object.assign(t.style, {
           position: 'absolute',
           left: '0px',
-          bottom: '0px',
+          bottom: 2vh,
           margin: '0',
           padding: '0',
           pointerEvents: 'none',
